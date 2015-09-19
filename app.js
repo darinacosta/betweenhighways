3424aadb586023b28588bc5bb545360dccefeeaf
darinacosta <darinjacosta@gmail.com>
2015-09-19 18:33:56 -0500

    css

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ var app = angular.module("app", ["ngRoute", "ngSanitize"])
       controllerUrl: 'components/home/homeCtrl',
       templateUrl: 'components/home/home.html'
   })
+   .when("/game", {
+      //controller: 'homeCtrl', 
+      //controllerUrl: 'components/home/homeCtrl',
+      templateUrl: 'components/game/game.html'
+  })
+  .when("/germancoast", {
+    //controller: 'homeCtrl', 
+    //controllerUrl: 'components/home/homeCtrl',
+    templateUrl: 'components/germancoast/germancoast.html'
+  })
   .otherwise({redirectTo: "/"});
 });
 
